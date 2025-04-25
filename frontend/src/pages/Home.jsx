3d6e3f9f8f27214429bbd2d11c4e36a6298f5cb2
mouhammad khoder <mhamadabdrazak555@gmail.com>
2025-04-25 23:36:49 +0300

    Update Home.jsx

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,6 +1,6 @@
 // eslint-disable-next-line no-unused-vars
 import React from 'react'
-import Hero from '../componens/hero'
+import Hero from '../componens/Hero'
 import LatestCollection from '../componens/LatestCollection'
 import BestSeller from '../componens/BestSeller'
 import OurPolicy from '../componens/OurPolicy'
